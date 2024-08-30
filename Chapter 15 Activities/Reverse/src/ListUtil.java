57ff335a1236ca92d176a9dfb9b07183e6728593
mjmares <142510831+mjmares@users.noreply.github.com>
2024-08-30 14:14:16 -0500

    d

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * This class supplies a utility method to reverse the entries in a linked list.
@@ -12,6 +13,29 @@ public class ListUtil
     */
     public static void reverse(LinkedList<String> strings)
     {
-        ...
+        ListIterator<String> firstIterator = strings.listIterator();
+        ListIterator<String> lastIterator = strings.listIterator();
+
+        String currentStr = firstIterator.next(); 
+        String lastStr = "";
+
+        // Move last to last
+        while(lastIterator.hasNext()) {
+            lastStr = lastIterator.next();
+        }
+
+        for(int i = 0; i <= (strings.size() - 1) / 2; i++) {
+            
+            int switchIndex = Math.abs(i - strings.size()) - 1;
+            
+            System.out.println(currentStr + " " + lastStr);
+
+            firstIterator.set(lastStr);
+            lastIterator.set(currentStr);
+
+            currentStr = firstIterator.next(); 
+            lastStr = lastIterator.previous();
+        }
+
     }
 }
