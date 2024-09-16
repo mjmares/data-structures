3e5440bd06960de75b58051aba060bf6dbdd3b1c
mjmares <mjmares@stu.naperville203.org>
2024-09-15 20:07:42 -0500

    String Length Map

--- Chapter 15 Activities/StringLengthMap/src/StringLengthMap.java
+++ Chapter 15 Activities/StringLengthMap/src/StringLengthMap.java
@@ -10,30 +10,41 @@ import java.io.*;
  */
 public class StringLengthMap
 {
-    public static void main(String[] args) throws FileNotFoundException
+    public static void main(String[] args)
     {
-        String filename = "src/test1.txt";
+        String filename = System.getProperty("user.dir") + "\\src\\test1.txt";
 
         try (Scanner in = new Scanner(new File(filename)))
         {
 
             // Create your map here
-            
+            Map<Character, Set<String>> firstLetterMap = new TreeMap<>();
 
             while (in.hasNext())
             {
                 String word = clean(in.next());
-                Integer len = word.length();
 
                 // Update the map here
                 // Modify Worked Example 15.1
-                
+                if (!word.isEmpty()) {
+                    char firstLetter = word.charAt(0);
 
-
+                    // Check if the map contains the first letter, if not, initialize it
+                    Set<String> wordSet = firstLetterMap.get(firstLetter);
+                    if (wordSet == null) {
+                        wordSet = new TreeSet<>();
+                        firstLetterMap.put(firstLetter, wordSet);
+                    }
+                wordSet.add(word);
+                }
             }
 
             // Print the strings, in increasing order of their length
             // Use this format: 1: i, a, i
+            for (Map.Entry<Character, Set<String>> entry : firstLetterMap.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+            
         } catch (FileNotFoundException e)
         {
             System.out.println("Cannot open: " + filename);
