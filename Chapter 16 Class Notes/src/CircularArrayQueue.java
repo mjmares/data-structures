37f930d0896015f4ff1f32630bce8b9dd855b5e9
mjmares <142510831+mjmares@users.noreply.github.com>
2024-10-01 14:17:40 -0500

    16 activitins

--- Chapter 16 Class Notes/src/CircularArrayQueue.java
+++ Chapter 16 Class Notes/src/CircularArrayQueue.java
@@ -6,51 +6,63 @@ import java.util.NoSuchElementException;
 public class CircularArrayQueue
 {
     private Object[] elements;
+    private int head;
+    private int tail;
+    private int currentSize;
     //private data
-
-
 
     /**
         Constructs an empty queue.
     */
-
-
-
-
-
+    public CircularArrayQueue() {
+        final int INIT_SIZE = 5;
+        this.elements = new Object[INIT_SIZE];
+        this.head = 0;
+        this.tail = 0;
+        this.currentSize = 0;
+    }
 
     /**
         Checks whether this queue is empty.
         @return true if this queue is empty
     */
-
-
-
+    public boolean empty() {
+        return currentSize == 0;
+    }
 
     /**
         Adds an element to the tail of this queue.
         @param newElement the element to add
     */
-
-
-
-
+    public void add(Object element) {
+        this.currentSize++;
+        this.elements[this.tail] = element;
+        this.tail++;
+        this.tail %= this.elements.length;
+        growIfNecessary();
+    }
 
     /**
         Removes an element from the head of this queue.
         @return the removed element
     */
+    public Object remove() {
+        if(this.empty()) {
+            throw new NoSuchElementException();
+        }
 
-
-
-
+        this.currentSize--;
+        Object element = this.elements[this.head];
+        this.head = (this.head + 1)  % this.elements.length;
+        return element;
+    }
 
     /**
         Grows the element array if the current size equals the capacity.
     */
     private void growIfNecessary()
     {
-        /*
+        
         if(this.currentSize == this.elements.length)
         {
             Object[] newElements = new Object[2 * this.elements.length];
@@ -62,10 +74,7 @@ public class CircularArrayQueue
             this.head = 0;
             this.tail = this.currentSize;
         }
-        */
+        
     }
-
-
-
 
 }//CircularArrayQueue
